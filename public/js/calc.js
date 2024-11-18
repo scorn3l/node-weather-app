0ce69a3aacf6159c2d59c632ab5a472347d5d6d1
sCorn3l <leo4rnnell@gmail.com>
2024-11-19 00:18:28 +0200

    Schimbat seva la calculeator

--- public/js/calc.js
+++ public/js/calc.js
@@ -34,46 +34,34 @@ calcBtn1.onclick = () => {
 }
 
 calcBtn2.onclick = () => {
-    console.log('test')
     const pZeroInput = document.getElementById(`p-zero-value1`).value
-    const roInput = document.getElementById(`ro-value1`).value
     const HInput1 = document.getElementById(`H-value1`).value
     const h1Input1 = document.getElementById(`h1-value1`).value
     const h2Input1 = document.getElementById(`h2-value1`).value
+    const T2Input1 = document.getElementById(`T2-value1`).value
+    const T1Input1 = document.getElementById(`T1-value1`).value
     const HInput2 = document.getElementById(`H-value2`).value
     const h1Input2 = document.getElementById(`h1-value2`).value
     const h2Input2 = document.getElementById(`h2-value2`).value
+    const T2Input2 = document.getElementById(`T2-value2`).value
+    const T1Input2 = document.getElementById(`T1-value2`).value
 
-    const HInput3 = document.getElementById(`H-value3`).value
-    const h1Input3 = document.getElementById(`h1-value3`).value
-    const h2Input3 = document.getElementById(`h2-value3`).value
 
-    n1 = (pZeroInput / pZeroInput * roInput * 9.81)*(h1Input1/h2Input1)
-    n2 = (pZeroInput / pZeroInput * roInput * 9.81)*(h1Input2/h2Input2)
-    n3 = (pZeroInput / pZeroInput * roInput * 9.81)*(h1Input3/h2Input3)
+    n1 = (pZeroInput / (pZeroInput - (1000 * 9.81 * HInput1 )))*(h1Input1/h2Input1)*(T2Input1/T1Input1)
+    n2 = (pZeroInput / (pZeroInput - (1000 * 9.81 * HInput2 )))*(h1Input2/h2Input2)*(T2Input2/T1Input2)
 
-    nmed = (n1+n2+n3)/3
+    nmed = (n1+n2)/2
     
     delta_n1 = Math.abs(n1-nmed)
     delta_n2 = Math.abs(n2-nmed)
-    delta_n3 = Math.abs(n3-nmed)
-
-    delta_nmed = (delta_n1+delta_n2+delta_n3)/3
     
     eroare_n1_delta_n1 = delta_n1/n1
     eroare_n2_delta_n2 = delta_n2/n2
-    eroare_n3_delta_n3 = delta_n3/n3
 
-    eroare_n1_delta_med = delta_nmed/n1
-    eroare_n2_delta_med = delta_nmed/n2
-    eroare_n3_delta_med = delta_nmed/n3
-
-    eroare_nmed = delta_nmed/nmed
 
     result_text = `------------------<br>
     n (1) = ${n1} <br>
-    n (2) = ${n2} <br>
-    n (3) = ${n3} <br><br>
+    n (2) = ${n2} <br><br>
 
     n (med) = ${nmed} <br><br>
     
@@ -81,24 +69,13 @@ calcBtn2.onclick = () => {
 
     ( | n (1) - n (med) | )<br><br>
     ▲n (1) = ${delta_n1} <br>
-    ▲n (2) = ${delta_n2} <br>
-    ▲n (3) = ${delta_n3} <br><br>
-
-    ▲n (med) = ${delta_nmed} <br><br>
+    ▲n (2) = ${delta_n2} <br><br>
 
     ------------------<br><br>
 
     ( ▲n (1) / (n1) )<br><br>
     E (1) = ${eroare_n1_delta_n1} <br>
-    E (2) = ${eroare_n2_delta_n2} <br>
-    E (3) = ${eroare_n3_delta_n3} <br><br>
-
-    ( ▲n (med) / (n1) )<br><br>
-    E (1) = ${eroare_n1_delta_med} <br>
-    E (2) = ${eroare_n2_delta_med} <br>
-    E (3) = ${eroare_n3_delta_med} <br><br>
-
-    E (med) = ${eroare_nmed} <br><br>
+    E (2) = ${eroare_n2_delta_n2} <br><br>
     
     ------------------<br>
     `
